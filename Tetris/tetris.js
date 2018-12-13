c707ca30d81eaec8aa4b17577c6ed700153c422a
yavuz <ygugen@windowslive.com>
2018-12-13 09:49:33 +0300

    Mobile Contorller

--- Tetris/tetris.js
+++ Tetris/tetris.js
@@ -289,7 +289,7 @@ function CONTROL(event) {
 function CONTROLMOBILE (event) {
   var x = event.touches[0].clientX;
   var y = event.touches[0].clientY;
-  document.getElementById("demo").innerText = x + ", " + y;
+  document.getElementById("demo").innerHTML = x + ", " + y;
 }
 
 // drop the piece every 1sec
