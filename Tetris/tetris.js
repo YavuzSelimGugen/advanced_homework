44c607fa9030ee3e20efefe32dea4c3333980bf1
Yavuz Selim <ygugen@windowslive.com>
2018-12-17 15:11:25 +0300

    Mobile controller.

--- Tetris/tetris.js
+++ Tetris/tetris.js
@@ -278,9 +278,49 @@ let p = randomPiece();
 
 
 // CONTROL the piece
-
 document.addEventListener("keydown", CONTROL);
-document.addEventListener("ontouchmove", CONTROLMOBILE);
+// For mobile:
+var touchX;
+var touchY;
+var touchId;
+
+document.body.addEventListener('touchstart', function (e) {
+  // e.preventDefault();
+  touchX = e.touches[0].pageX;
+  touchY = e.touches[0].pageY;
+  touchId = e.touches[0].identifier;
+});
+
+document.body.addEventListener('touchend', function (e) {
+  // e.preventDefault();
+  var touchEndX;
+  var touchEndY;
+  var touch = e.changedTouches.item(0);
+  try {
+    touchEndX = touch.pageX;
+    touchEndY = touch.pageY;
+  } catch (err) {
+    console.log(arr);
+    return;
+  }
+
+  var difX = (touchEndX - touchX);
+  var difY = (touchEndY - touchY);
+  console.log("DiffX: "+difX)
+  console.log("DiffY: "+difY)
+  if (difX > 50) {
+    p.moveRight();
+  }
+  else if(difX < -50) {
+    p.moveLeft();
+  }
+  else if(difY < -50 ) {
+    p.moveDown();
+  }
+  else if(difX == 0 && difY == 0) {
+    p.rotate();
+  }
+})
 
 function CONTROL(event) {
     if (event.keyCode == 37) {
